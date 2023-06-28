40e2993727af227ca926dca943636d3517af1030
Only-bottle <bmlee@nota.ai>
2023-06-29 00:28:20 +0900

    UPDATE: 5.2 Custom Fields

--- users/models.py
+++ users/models.py
@@ -8,4 +8,4 @@ class User(AbstractUser):
     first_name = models.CharField(max_length=150, editable=False)
     last_name = models.CharField(max_length=150, editable=False)
     name = models.CharField(max_length=150)
-    is_host = models.BooleanField()
+    is_host = models.BooleanField()  # Not Null과 Null의 중요성
