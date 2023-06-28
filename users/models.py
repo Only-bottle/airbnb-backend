5d01688b2779d8a08d8c97ad0635db8f68622ee8
Only-bottle <bmlee@nota.ai>
2023-06-29 00:25:06 +0900

    ADD: 5.2 Custom Fields

--- users/models.py
+++ users/models.py
@@ -3,5 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 
 # Django가 사용하는 user를 사용 -> AbstractUser
+# Django의 기본 user를 사용하지 않고 내가 만든 모델을 사용할 거라고 알려줘야 함
 class User(AbstractUser):
-    pass
+    first_name = models.CharField(max_length=150, editable=False)
+    last_name = models.CharField(max_length=150, editable=False)
+    name = models.CharField(max_length=150)
+    is_host = models.BooleanField()
