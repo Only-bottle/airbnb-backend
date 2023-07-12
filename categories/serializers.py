47fe1a773a28dcf32152f48a9d055c2af4fa8147
Only-bottle <bmlee@nota.ai>
2023-07-12 22:16:16 +0900

    ADD: 10.7 update()

--- categories/serializers.py
+++ categories/serializers.py
@@ -18,3 +18,10 @@ class CategorySerializer(serializers.Serializer):
 
     def create(self, validated_data):
         return Category.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        # instance -> db에서 가져온 데이터
+        instance.name = validated_data.get("name", instance.name)
+        instance.kind = validated_data.get("kind", instance.kind)
+        instance.save()
+        return instance
--- categories/views.py
+++ categories/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
 from .models import Category
 from .serializers import CategorySerializer
 
@@ -23,13 +24,29 @@ def categories(request):
             return Response(serializer.errors)
 
 
-@api_view()
+@api_view(["GET", "PUT"])
 def category(request, pk):
-    """
-    Django에서 JSON으로 번역하고 싶으면 CategorySerializer에 category를 넘기고
+    # Django에서 JSON으로 번역하고 싶으면 CategorySerializer에 category를 넘기고
+    try:
+        category = Category.objects.get(pk=pk)
+    except Category.DoesNotExist:
+        raise NotFound
     
-    """
-    category = Category.objects.get(pk=pk)
-    serializer = CategorySerializer(category)
+    if request.method == "GET":
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
 
-    return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = CategorySerializer(
+            category,
+            data=request.data,
+            partial=True,
+        )
+        if serializer.is_valid():
+            # 여기서 save를 하면 create를 하지 않는다.
+            # db에서 가져온 category와 유저로 받은 데이터가 있기 때문에
+            # 즉, 업데이트라고 판단함.
+            updated_category = serializer.save()
+            return Response(CategorySerializer(updated_category).data)
+        else:
+            return Response(serializer.errors)
