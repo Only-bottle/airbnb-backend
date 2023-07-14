c61408e99bec2f6ad71033738d77240a6914e7fc
Only-bottle <bmlee@nota.ai>
2023-07-15 00:31:31 +0900

    ADD: 11.7 Room Category

--- rooms/views.py
+++ rooms/views.py
@@ -1,10 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.status import HTTP_204_NO_CONTENT
 from rest_framework.response import Response
-from rest_framework.exceptions import NotFound, NotAuthenticated
+from rest_framework.exceptions import NotFound, NotAuthenticated, ParseError
 
 from .models import Room, Amenity
 from .serializers import RoomListSerializer, RoomDetailSerializer, AmenitySerializer
+from categories.models import Category
 
 
 class Rooms(APIView):
@@ -17,7 +18,20 @@ class Rooms(APIView):
         if request.user.is_authenticated:
             serializer = RoomDetailSerializer(data=request.data)
             if serializer.is_valid():
-                room = serializer.save(owner=request.user)  # 해당 serializer의 create 함수에 validated_data가 추가된다.
+                # category는 create함수에 전달되지 않는다 -> read_only로 설정했기 떄문에
+                category_pk = request.data.get("category")
+                if not category_pk:
+                    raise ParseError
+                try:
+                    category = Category.objects.get(pk=category_pk)
+                    if category.kind == Category.CategoryKindChoices.EXPERIENCES:
+                        raise ParseError
+                except Category.DoesNotExist:
+                    raise ParseError
+                room = serializer.save(
+                    owner=request.user,
+                    category=category,
+                )  # 해당 serializer의 create 함수에 validated_data가 추가된다.
                 serializer = RoomDetailSerializer(room)
                 return Response(serializer.data)
             else:
