86197132e48989d669bc94b97650462ddb6163e0
Only-bottle <bmlee@nota.ai>
2023-07-16 01:52:30 +0900

    ADD: 11.23 Validate Booking

--- bookings/serializers.py
+++ bookings/serializers.py
@@ -23,11 +23,28 @@ class CreateRoomBookingSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("Can't book in the past!")
         return value
 
-    def validate_check_out(self, value):
+    def validate_check_out(self, value):  # 각 field에 대해서 validate
         now = timezone.localtime(timezone.now()).date()
         if now > value:
             raise serializers.ValidationError("Can't book in the past!")
         return value
+    
+    def validate(self, data):  # 전체 field에 대해서 validate 할 수 있음
+        if data["check_out"] <= data["check_in"]:
+            raise serializers.ValidationError(
+                "Check in should be smaller than check out."
+            )
+        # Want: 05-10 ~ 05-20
+        # Existing: 05-02 ~ 05-15
+        
+        if Booking.objects.filter(
+            check_in__lte=data["check_out"],  # in날짜가 out보다 작거나 같은지
+            check_out__gte=data["check_in"],  # out날짜가 in보다 크거나 같은지
+        ).exists():
+            raise serializers.ValidationError(
+                "Those (or some) of those dates are already taken."
+            )
+        return data
 
 
 class PublicBookingSerializer(serializers.ModelSerializer):
