ab001dca17ed5412e69ca7e78d9def7b2e28a233
Only-bottle <bmlee@nota.ai>
2023-07-16 01:37:36 +0900

    ADD: 11.23 Create a Booking

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import Rooms, RoomDetail, RoomReviews, RoomPhotos, RoomAmenities, Amenities, AmenityDetail
+from .views import Rooms, RoomDetail, RoomReviews, RoomPhotos, RoomBookings, RoomAmenities, Amenities, AmenityDetail
 
 
 urlpatterns = [
@@ -9,7 +9,7 @@ urlpatterns = [
     path("<int:pk>/reviews", RoomReviews.as_view()),
     path("<int:pk>/amenities", RoomAmenities.as_view()),
     path("<int:pk>/photos", RoomPhotos.as_view()),
-    path("<int:pk>/bookings", RoomPhotos.as_view()),
+    path("<int:pk>/bookings", RoomBookings.as_view()),
     path("amenities/", Amenities.as_view()),
     path("amenities/<int:pk>", AmenityDetail.as_view()),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -19,7 +19,7 @@ from categories.models import Category
 from reviews.serializers import ReviewSerializer
 from medias.serializers import PhotoSerializer
 from bookings.models import Booking
-from bookings.serializers import PublicBookingSerializer
+from bookings.serializers import PublicBookingSerializer, CreateRoomBookingSerializer
 
 
 class Rooms(APIView):
@@ -249,6 +249,14 @@ class RoomBookings(APIView):
         )
         serializer = PublicBookingSerializer(bookings, many=True)
         return Response(serializer.data)
+
+    def post(self, request, pk):
+        room = self.get_object(pk)
+        serializer = CreateRoomBookingSerializer(data=request.data)
+        if serializer.is_valid():
+            return Response({"ok": True})
+        else:
+            return Response(serializer.errors)
 
 
 class Amenities(APIView):
