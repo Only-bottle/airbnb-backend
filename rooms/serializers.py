9b4028de4b046f7cc6d9e7a8e4cc749ec757531f
Only-bottle <bmlee@nota.ai>
2023-07-16 00:19:33 +0900

    ADD: 11.21 is_liked

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -5,6 +5,7 @@ from users.serializers import TinyUserSerializer
 from categories.serializers import CategorySerializer
 from reviews.serializers import ReviewSerializer
 from medias.serializers import PhotoSerializer
+from wishlists.models import Wishlist
 
 
 class AmenitySerializer(serializers.ModelSerializer):
@@ -23,6 +24,7 @@ class RoomDetailSerializer(serializers.ModelSerializer):
     rating = serializers.SerializerMethodField()
     # 연결되어 있는 serializer 클래스에서 메소드를 호출하여 값을 가져올 수 있는 읽기 전용 필드이다.
     is_owner = serializers.SerializerMethodField()
+    is_liked = serializers.SerializerMethodField()
     photos = PhotoSerializer(many=True, read_only=True)
 
     class Meta:
@@ -35,6 +37,13 @@ class RoomDetailSerializer(serializers.ModelSerializer):
     def get_is_owner(self, room):
         request = self.context["request"]  # view에서 전달한 context를 확인
         return room.owner == request.user
+
+    def get_is_liked(self, room):
+        request = self.context["request"]
+        return Wishlist.objects.filter(  # room을 보고 있는 유저가 소요한 wishlist를 찾는다.
+            user=request.user,
+            rooms__pk=room.pk,  # wishlist에 해당 room이 들어있는지
+        ).exists()
 
 
 class RoomListSerializer(serializers.ModelSerializer):
