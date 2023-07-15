d7f9a0fb67b334547dd0fd69cfb3977797d60465
Only-bottle <bmlee@nota.ai>
2023-07-15 23:53:15 +0900

    ADD: 11.19 Wishlists

---
+++ wishlists/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from .views import Wishlists
+
+urlpatterns = [
+    path("", Wishlists.as_view()),
+]
--- wishlists/views.py
+++ wishlists/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Wishlist
+from .serializers import WishlistSerializer
+
+
+class Wishlists(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        all_wishlists = Wishlist.objects.filter(user=request.user)  # 유저가 가지고 있는 wishlist 보여주기
+        serializer = WishlistSerializer(
+            all_wishlists,
+            many=True,
+            context={"request": request},  # RoomSerializer에서 필요함.
+        )
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = WishlistSerializer(data=request.data)
+        if serializer.is_valid():
+            wishlist = serializer.save(
+                user=request.user,
+            )
+            serializer = WishlistSerializer(wishlist)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
