8a9c82ad687fbbfefa5e83ebb41ea2cef0d7cffb
Only-bottle <bmlee@nota.ai>
2023-07-23 01:59:28 +0900

    ADD: 12.2 Change Password

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.Users.as_view()),
     path("me", views.Me.as_view()),
+    path("change-password", views.ChangePassword.as_view()),
+    path("@<str:username>", views.PublicUser.as_view()),
 ]
