972c9804174fb8e85f1835beed9757422e98152d
Only-bottle <bmlee@nota.ai>
2023-07-23 01:41:41 +0900

    ADD: 12.1 Create User

--- users/views.py
+++ users/views.py
@@ -1,7 +1,10 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+from rest_framework.exceptions import ParseError, NotFound
 from rest_framework.permissions import IsAuthenticated
+
+from users.models import User
 from . import serializers
 
 
@@ -23,6 +26,22 @@ class Me(APIView):
         )
         if serializer.is_valid():
             user = serializer.save()
+            serializer = serializers.PrivateUserSerializer(user)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+class Users(APIView):
+    def post(self, request):
+        password = request.data.get("password")
+        if not password:
+            raise ParseError("Password is required.")
+        serializer = serializers.PrivateUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            user.set_password(password)
+            user.save()
             serializer = serializers.PrivateUserSerializer(user)
             return Response(serializer.data)
         else:
