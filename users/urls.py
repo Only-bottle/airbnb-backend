685e0c12c6638fa727956a285fac6fe534fda74f
Only-bottle <bmlee@nota.ai>
2023-07-23 19:12:14 +0900

    ADD: 15.4 JWT Encode

--- users/urls.py
+++ users/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path("log-in", views.LogIn.as_view()),
     path("log-out", views.LogOut.as_view()),
     path("token-login", obtain_auth_token),
+    path("jwt-login", views.JWTLogIn.as_view()),
     path("@<str:username>", views.PublicUser.as_view()),
 ]
