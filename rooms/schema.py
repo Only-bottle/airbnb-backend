751dfd5c94d4b9456042baf5876861d545c1a163
Only-bottle <bmlee@nota.ai>
2023-07-23 15:50:13 +0900

    ADD: 14.9 Permissions

--- rooms/schema.py
+++ rooms/schema.py
@@ -2,12 +2,14 @@ import strawberry
 from typing import List, Optional
 from . import types
 from . import queries
+from common.permissions import OnlyLoggedIn
 
 
 @strawberry.type
 class Query:
     all_rooms: List[types.RoomType] = strawberry.field(
         resolver=queries.get_all_rooms,
+        permission_classes=[OnlyLoggedIn],
     )
     room: Optional[types.RoomType] = strawberry.field(
         resolver=queries.get_room
