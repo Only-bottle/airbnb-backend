40c95f50de3d26b3710cfdf92c323b1513cc69c6
Only-bottle <bmlee@nota.ai>
2023-07-23 15:37:20 +0900

    ADD: 14.8 Custom Resolvers

--- rooms/schema.py
+++ rooms/schema.py
@@ -1,11 +1,14 @@
 import strawberry
-import typing
+from typing import List, Optional
 from . import types
 from . import queries
 
 
 @strawberry.type
 class Query:
-    all_rooms: typing.List[types.RoomType] = strawberry.field(
+    all_rooms: List[types.RoomType] = strawberry.field(
         resolver=queries.get_all_rooms,
+    )
+    room: Optional[types.RoomType] = strawberry.field(
+        resolver=queries.get_room
     )
--- rooms/types.py
+++ rooms/types.py
@@ -1,13 +1,15 @@
-from typing import List
+from typing import List, Optional
 from django.conf import settings
 
 import strawberry
 import strawberry.django 
 from strawberry import auto
+from strawberry.types import Info
 
 from . import models
 from users.types import UserType
 from reviews.types import ReviewType
+from wishlists.models import Wishlist
 
 
 @strawberry.django.type(models.Room)
@@ -18,7 +20,7 @@ class RoomType:
     owner: "UserType"
 
     @strawberry.field
-    def reviews(self, page: int) -> List["ReviewType"]:
+    def reviews(self, page: Optional[int] = 1) -> List["ReviewType"]:
         page_size = settings.PAGE_SIZE
         start = (page - 1) * page_size
         end = start + page_size
@@ -27,3 +29,14 @@ class RoomType:
     @strawberry.field
     def rating(self) -> str:
         return self.rating()
+
+    @strawberry.field
+    def is_owner(self, info: Info) -> bool:
+        return self.owner == info.context.request.user
+
+    @strawberry.field
+    def is_liked(self, info: Info) -> bool:
+        return Wishlist.objects.filter(
+            user=info.context.request.user,
+            rooms__pk=self.pk,
+        ).exists()
