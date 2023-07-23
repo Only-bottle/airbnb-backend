7c5ef7ad3b9a64ef25a871b64ae76f7d515b2e57
Only-bottle <bmlee@nota.ai>
2023-07-23 20:28:11 +0900

    ADD: 16.2 Amenities Test

--- rooms/tests.py
+++ rooms/tests.py
@@ -1,7 +1,41 @@
 from rest_framework.test import APITestCase
 
+from . import models
+
 
 class TestAmenities(APITestCase):
-    def test_two_plus_two(self):
+    NAME = "Amenity Test"
+    DESC = "Amenity Des"
 
-        self.assertEqual(2 + 2, 4, "The math is wrong.")
+    def setUp(self):  # 다른 모든 테스트들이 실행되기 전에 수행됨
+        models.Amenity.objects.create(
+            name=self.NAME,
+            description=self.DESC,
+        )
+
+    def test_all_amenities(self):
+        response = self.client.get("/api/v1/rooms/amenities/")
+        data = response.json()
+        print(data)
+
+        self.assertEqual(
+            response.status_code,
+            200,
+            "Status code isn't 200.",
+        )
+        self.assertIsInstance(
+            data,
+            list,
+        )
+        self.assertEqual(
+            len(data),
+            1,
+        )
+        self.assertEqual(
+            data[0]["name"],
+            self.NAME,
+        )
+        self.assertEqual(
+            data[0]["description"],
+            self.DESC,
+        )
