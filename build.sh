7980e8f8e5435e09cd592d01a1c39b68498efc7c
Only-bottle <bmlee@nota.ai>
2023-08-11 00:09:25 +0900

    UPDATE: 23.0 Render Blueprint

--- build.sh
+++ build.sh
@@ -2,12 +2,7 @@
 # exit on error
 set -o errexit
 
-pip install --upgrade pip
-pip install poetry==1.2.0
-rm poetry.lock
-poetry lock
-python -m poetry install
-pip install --force-reinstall -U setuptools
+poetry install
 
 python manage.py collectstatic --no-input
 python manage.py migrate
